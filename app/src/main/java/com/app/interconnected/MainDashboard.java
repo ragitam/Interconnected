4286ee874006de8cfa745ff988c21ddaea7c60b1
DIFAA <afidirawan@students.telkomuniversity.ac.id>
2016-11-09 09:08:24 +0700

    nambah cardview + recycleview

---
+++ app/src/main/java/com/app/interconnected/MainDashboard.java
@@ -0,0 +1,60 @@
+package com.app.interconnected;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainDashboard extends AppCompatActivity {
+
+    private RecyclerView rvView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<String> dataSet;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_dashboard);
+        dataSet = new ArrayList<>();
+        initDataset();
+
+        rvView = (RecyclerView) findViewById(R.id.recyclerView_kegiatan);
+        rvView.setHasFixedSize(true);
+
+        /**
+         * Kita menggunakan LinearLayoutManager untuk list standar
+         * yang hanya berisi daftar item
+         * disusun dari atas ke bawah
+         */
+        layoutManager = new LinearLayoutManager(this);
+        rvView.setLayoutManager(layoutManager);
+
+        adapter = new RecyclerViewAdapter(dataSet);
+        rvView.setAdapter(adapter);
+
+    }
+
+    private void initDataset(){
+
+        /**
+         * Tambahkan item ke dataset
+         * dalam prakteknya bisa bermacam2
+         * tidak hanya String seperti di kasus ini
+         */
+        dataSet.add("Karin");
+        dataSet.add("Ingrid");
+        dataSet.add("Helga");
+        dataSet.add("Renate");
+        dataSet.add("Elke");
+        dataSet.add("Ursula");
+        dataSet.add("Erika");
+        dataSet.add("Christa");
+        dataSet.add("Gisela");
+        dataSet.add("Monika");
+
+    }
+}
