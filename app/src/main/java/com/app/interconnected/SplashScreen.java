3dbc98574ef236cbd7383ea6cea6c172136ef4eb
DIFAA <afidirawan@students.telkomuniversity.ac.id>
2016-12-04 17:53:52 +0700

    Nambah Splash Screen

---
+++ app/src/main/java/com/app/interconnected/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.app.interconnected;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+        Thread thread = new Thread() {
+            public void run() {
+                try {
+                    sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                    finish();
+                }
+            }
+        };
+        thread.start();
+    }
+}
